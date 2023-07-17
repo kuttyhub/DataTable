4335f01fa984bc29e85d44f98d8d7a5b7de31104
kuttyhub <nishanth1216@gmail.com>
2023-07-17 23:09:42 +0530

    Added mock data and Api

---
+++ app/api/order-history/route.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { promises as fs } from "fs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const jsonDirectory = path.join(process.cwd(), "json");
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  return NextResponse.json(fileContents);
+}
